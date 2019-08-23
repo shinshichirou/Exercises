78e449cadc06ceba0ab0e6a37cb413fb3d1a413e
Igor Tudoran <igortudoran@me.com>
2019-08-23 12:03:42 +0300

    Initial Commit

---
+++ Exercises/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercises
+//
+//  Created by Igor Tudoran on 8/23/19.
+//  Copyright © 2019 DIGIS IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
